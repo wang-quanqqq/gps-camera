d91ff8c00b76472d4e3069784d07fdd431fa1f35
wang-quanqqq <592887735@qq.com>
2026-05-18 07:08:03 +0800

    fix: remove aliyun mirror, causing 502 Bad Gateway errors

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,5 @@
 pluginManagement {
     repositories {
-        maven { url = uri("https://maven.aliyun.com/repository/public") }
         google()
         mavenCentral()
         gradlePluginPortal()
@@ -9,7 +8,6 @@ pluginManagement {
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
-        maven { url = uri("https://maven.aliyun.com/repository/public") }
         google()
         mavenCentral()
     }
